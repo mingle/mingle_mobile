caa6dec94d7aa0273032c095d604d8c2465c5d86
vinayd <vinayd@thoughtworks.com>
2013-10-11 14:12:19 -0700

    removed credentials for api and added an example

--- onmobile/config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.url = 'http://mingle.mingle-staging.thoughtworks.com';
-config.username = 'apiuser';
-config.password = 'tw0rk3r!';
-config.projectName = "ratchet";
-
-module.exports = config;
